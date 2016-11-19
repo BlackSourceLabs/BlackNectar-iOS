7e1e8862d16ffcae81348526399e175e370ff274
Cord <Cordhernandez@yahoo.com>
2016-11-19 14:10:42 -0800

    adding mapKit
    adding mapKit

---
+++ BlackNectar/BlackNectar/StoresMapViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  BlackNectar
+//
+//  Created by Cordero Hernandez on 11/19/16.
+//  Copyright © 2016 Black Whole. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import MapKit
+import CoreLocation
+
+class StoresMapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager: CLLocationManager!
+    
+    var currentLatitude = ""
+    var currentLongitude = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let userLocation: CLLocation = locations[0]
+        let latitude = userLocation.coordinate.latitude
+        let longitude = userLocation.coordinate.longitude
+        let latDelta: CLLocationDegrees = 0.05
+        let longDelta: CLLocationDegrees = 0.05
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        currentLatitude = "\(location.latitude)"
+        currentLongitude = "\(location.longitude)"
+        
+        self.mapView.setRegion(region, animated: true)
+        
+        locationManager.stopUpdatingLocation()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

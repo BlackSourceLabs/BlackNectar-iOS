d92d0c3f22a1d15dc3dfa8f72cfb4fbc886bc822
kevintbradbury <kevin.t.bradbury@gmail.com>
2016-11-30 13:30:04 -0800

    able to get currentLocation in StoresTableViewController

--- BlackNectar/BlackNectar/StoresTableViewController.swift
+++ BlackNectar/BlackNectar/StoresTableViewController.swift
@@ -10,49 +10,52 @@ import UIKit
 import Foundation
 import CoreLocation
 
-class StoresTableViewController: UITableViewController {
-
-    private var stores: [StoresInfo] = []
+class StoresTableViewController: UITableViewController, CLLocationManagerDelegate {
+    
+    var stores: [StoresInfo] = []
+    var currentLocation = UserLocation().prepareForLocation()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         UserLocation().prepareForLocation()
-        //currentLocation = UserLocation().returnsLocation()
-        
-        
+            print("tableview current location is : \(currentLocation)")
+        SearchStores.searchForStoresLocations(near: currentLocation) { stores in
+            print("Search for stores, stores is : \(stores)")
+            self.stores = stores
+        }
+        print("stores is \(stores)")
     }
-
     
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-       
+        
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return 1
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "storeCell", for: indexPath)
         
         
-
+        
         return cell
     }
- 
-
-  
-
+    
+    
+    
+    
 }

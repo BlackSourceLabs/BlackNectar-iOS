0e46456fb375bfe0e14968f2f87f882d7396c642
Cord <Cordhernandez@yahoo.com>
2016-12-30 14:34:57 -0800

    removes Carthage/Checkouts

--- BlackNectar/Carthage/Checkouts/AromaThrift/AromaThrift/update-headers.sh
+++
@@ -1,4 +0,0 @@
-find AromaThrift -regex '.*.h' -print0 | xargs -0 sed -i  '' 's_.*import.*"TProtocol.h"_@import LibThrift;_'
-find AromaThrift -regex '.*.h' -print0 | xargs -0 sed -i  '' 's_.*import.*T.*.h"__'
-find AromaThrift -regex '.*.m' -print0 | xargs -0 sed -i  '' 's_.*import.*"TProtocol.h"_@import LibThrift;_'
-find AromaThrift -regex '.*.m' -print0 | xargs -0 sed -i  '' 's_.*import.*T.*.h"__'

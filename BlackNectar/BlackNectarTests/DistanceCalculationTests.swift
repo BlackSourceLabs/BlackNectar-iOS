0ed5512d6a49a9ffffe9d88e9ee39778f67e0cf4
Cord <Cordhernandez@yahoo.com>
2017-01-17 17:43:09 -0800

    adds Distance Calculation Tests

---
+++ BlackNectar/BlackNectarTests/DistanceCalculationTests.swift
@@ -0,0 +1,51 @@
+//
+//  DistanceCalculationTests.swift
+//  BlackNectar
+//
+//  Created by Cordero Hernandez on 1/16/17.
+//  Copyright © 2017 Black Whole. All rights reserved.
+//
+
+@testable import BlackNectar
+
+import CoreLocation
+import XCTest
+
+class DistanceCalculationTests: XCTestCase {
+    
+    func testGetDistance() {
+        
+        let usersLatitude: CLLocationDegrees = 40.6782
+        let usersLongitude: CLLocationDegrees = -73.9442
+        let userslocation = CLLocationCoordinate2D(latitude: usersLatitude, longitude: usersLongitude)
+        
+        let restaurantsLatitude: CLLocationDegrees = 34.14341
+        let restaurantsLongitude: CLLocationDegrees = -118.392
+        let restaurantsLocation = CLLocationCoordinate2D(latitude: restaurantsLatitude, longitude: restaurantsLongitude)
+        
+        XCTAssertNotNil(DistanceCalculation.getDistance(userLocation: userslocation, storeLocation: restaurantsLocation))
+        
+    }
+    
+    func testMilesToMeters() {
+        
+        let miles: Double = 100.00
+        
+        XCTAssertNotNil(DistanceCalculation.milesToMeters(miles: miles))
+        
+        
+    }
+    
+    func testMetersToMiles() {
+        
+        let meters: Double = 200.00
+        
+        XCTAssertNotNil(DistanceCalculation.milesToMeters(miles: meters))
+        
+    }
+    
+    
+    
+  
+    
+}

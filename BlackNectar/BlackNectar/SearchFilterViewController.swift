e72b418cc5fea54f12866072f765d5e5041db0ea
Cord <Cordhernandez@yahoo.com>
2017-02-06 13:19:47 -0800

    adds mapView methods
    + func loadStore()
    + func prepareMapView()
    + func loadStoresInMapView()
    + func populateStoreAnnotations()
    
    Adds Aroma Messages
    Adds Network Loading Indicator Code

--- BlackNectar/BlackNectar/SearchFilterViewController.swift
+++ BlackNectar/BlackNectar/SearchFilterViewController.swift
@@ -62,9 +62,161 @@ class SearchFilterViewController: UITableViewController, MKMapViewDelegate, CLLo
         
     }
     
+    private func loadStores() {
+        
+        UserLocation.instance.requestLocation { coordinate in
+            
+            self.loadStoresInMapView(at: coordinate)
+            
+        }
+        
+    }
     
-    
+    private func prepareMapView() {
+        
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        
+        guard let region = UserLocation.instance.currentRegion else {
+            
+            LOG.error("Failed to the Users Current Region")
+            return
+        }
+        
+        self.mapView.setRegion(region, animated: true)
+        
+    }
     
 }
+
+//MARK: Loading Stores into Search Filter Map View
+extension StoresMapViewControlelr {
+    
+    func loadStoresInMapView(at coordinate: CLLocationCoordinate2D) {
+        
+        startSpinningIndicator()
+        
+        let distanceInMeters = DistanceCalculation.milesToMeters(miles: Double(distance))
+        
+        SearchStores.searchForStoresLocations(near: coordinate, with: distanceInMeters) { stores in
+            
+            self.stores = stores
+            
+            
+            self.main.addOperation {
+                
+                self.populateStoreAnnotations()
+                self.stopSpinningIndicator()
+            }
+            
+        }
+        
+        if self.stores.isEmpty {
+            
+            self.makeNoteThatNoStoresFound(additionalMessage: "User is in the Search Filter Map View")
+            
+        }
+        
+    }
+    
+    func populateStoreAnnotations() {
+        
+        var annotations: [MKAnnotation] = []
+        
+        for store in stores {
+            
+            let annotation = createAnnotation(forStore: store)
+            annotations.append(annotation)
+        }
+        
+        mapView.addAnnotations(annotations)
+        mapView.removeNonVisibleAnnotations()
+    }
+    
+}
+
+//MARK: Aroma Messages
+extension SearchFilterViewController {
+    
+    func makeNoteThatNoStoresFound(additionalMessage: String = "") {
+        
+        LOG.warn("There are no stores around the users location (Stores loading result is 0)")
+        AromaClient.beginMessage(withTitle: "No stores loading result is 0")
+            .addBody("There are no stores around the users location (Stores loading result is 0 : \(additionalMessage)")
+            .withPriority(.high)
+            .send()
+        
+    }
+    
+    func userLocationInfoForAroma() {
+        
+        guard let userLocationForAroma = UserLocation.instance.currentCoordinate else { return }
+        
+        AromaClient.beginMessage(withTitle: "User Entered Map View")
+            .addBody("User Location is: \(userLocationForAroma)")
+            .withPriority(.low)
+            .send()
+        
+    }
+    
+}
+
+//MARK: Network Loading Indicator Code
+fileprivate extension SearchFilterViewController {
+    
+    
+    func startSpinningIndicator() {
+        
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        
+    }
+    
+    func stopSpinningIndicator() {
+        
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 

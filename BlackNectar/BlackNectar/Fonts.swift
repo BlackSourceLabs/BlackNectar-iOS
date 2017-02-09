253f0693dd3041a2acf80c26ebdb6cc6ac4854fd
Cord <Cordhernandez@yahoo.com>
2017-02-08 19:28:42 -0800

    adds Fonts.swift
    adds oxygenBold
    adds oxygenRegular

---
+++ BlackNectar/BlackNectar/Fonts.swift
@@ -0,0 +1,20 @@
+//
+//  Fonts.swift
+//  BlackNectar
+//
+//  Created by Cordero Hernandez on 2/8/17.
+//  Copyright © 2017 BlackSource. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Fonts {
+    
+    static let instance = Fonts()
+    private init() {}
+    
+    var oxygenBold = UIFont(name: "Oxygen-Bold", size: 16)
+    var oxygenRegular = UIFont(name: "Oxygen-Regular", size: 16)
+  
+}

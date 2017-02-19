7e9d36314772924e343951c3b13c3abe033b783d
Wellington Moreno <jwellington.moreno@gmail.com>
2017-02-18 16:00:55 -0800

    Refines user preferences

--- BlackNectar/UserPreferences.swift
+++ BlackNectar/UserPreferences.swift
@@ -20,13 +20,13 @@ class UserPreferences {
         
         get {
         
-            return defaultPreferences.double(forKey: "distanceFilter")
+            return defaultPreferences.double(forKey: Keys.searchRadius)
         
         }
         
         set (value){
         
-            defaultPreferences.set(value, forKey: "distanceFilter")
+            defaultPreferences.set(value, forKey: Keys.searchRadius)
             
         }
         
@@ -36,13 +36,13 @@ class UserPreferences {
         
         get {
             
-            return defaultPreferences.object(forKey: "showFarmersMarkets") as? Bool ?? true
+            return defaultPreferences.object(forKey: Keys.showFarmersMarkets) as? Bool ?? true
             
         }
         
         set (value){
             
-            defaultPreferences.set(value, forKey: "showFarmersMarkets")
+            defaultPreferences.set(value, forKey: Keys.showFarmersMarkets)
             
         }
         
@@ -52,13 +52,13 @@ class UserPreferences {
         
         get {
             
-            return defaultPreferences.object(forKey: "showStores") as? Bool ?? true
+            return defaultPreferences.object(forKey: Keys.showStores) as? Bool ?? true
             
         }
         
         set (value){
             
-            defaultPreferences.set(value, forKey: "showStores")
+            defaultPreferences.set(value, forKey: Keys.showStores)
             
         }
         
@@ -68,13 +68,13 @@ class UserPreferences {
         
         get {
             
-            return defaultPreferences.object(forKey: "showMyLocationSwitch") as? Bool ?? true
+            return defaultPreferences.object(forKey: Keys.useMyLocation) as? Bool ?? true
             
         }
         
         set (value){
             
-            defaultPreferences.set(value, forKey: "showMyLocationSwitch")
+            defaultPreferences.set(value, forKey: Keys.useMyLocation)
             
         }
         
@@ -84,7 +84,7 @@ class UserPreferences {
         
         get {
             
-            return defaultPreferences.object(forKey: "showUseZipCodeSwitch") as? Bool ?? true
+            return defaultPreferences.object(forKey: Keys.useZipCode) as? Bool ?? true
             
         }
         
@@ -96,4 +96,16 @@ class UserPreferences {
         
     }
     
+}
+
+fileprivate class Keys {
+    
+    private static let namespace = "tech.blacksource.blacknectar"
+    
+    static let searchRadius = namespace + "searchRadius"
+    static let showStores = namespace + "showStores"
+    static let showFarmersMarkets = namespace + "showFarmersMarkets"
+    static let useMyLocation = namespace + "useMyLocation"
+    static let useZipCode = namespace + "useZipCode"
+    static let zipCode = namespace + "zipCode"
 }

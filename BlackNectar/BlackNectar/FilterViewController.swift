d24343c90e180aea2395e11d06c513da9515b10a
Cord <Cordhernandez@yahoo.com>
2017-02-20 11:26:09 -0800

    adds IBOutlet zipCodeButton to FilterViewController

--- BlackNectar/BlackNectar/FilterViewController.swift
+++ BlackNectar/BlackNectar/FilterViewController.swift
@@ -27,6 +27,7 @@ class FilterViewController: UITableViewController, MKMapViewDelegate, CLLocation
     @IBOutlet weak var groceryStoresButton: UIButton!
     @IBOutlet weak var useMyLocationSwitch: UISwitch!
     @IBOutlet weak var useZipeCodeSwitch: UISwitch!
+    @IBOutlet weak var zipCodeButton: CustomButtonView!
     @IBOutlet weak var zipCodeLabel: UILabel!
     
     

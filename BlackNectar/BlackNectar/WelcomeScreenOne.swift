9135db95b27c34d07af22d166c3c7b913e31f5ba
Cord <Cordhernandez@yahoo.com>
2017-02-28 00:52:56 -0800

    Adds WelcomeScreenOne.swift

---
+++ BlackNectar/BlackNectar/WelcomeScreenOne.swift
@@ -0,0 +1,31 @@
+//
+//  WelcomeScreenOne.swift
+//  BlackNectar
+//
+//  Created by Cordero Hernandez on 2/23/17.
+//  Copyright © 2017 BlackSource. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WelcomeScreenOne: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+}

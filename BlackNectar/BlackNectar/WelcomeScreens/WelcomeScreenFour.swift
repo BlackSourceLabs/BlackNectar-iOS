a30d9e413495dbd63102315acb099768a891efbd
Wellington Moreno <jwellington.moreno@gmail.com>
2017-03-03 10:23:46 -0800

    Reorganizes welcome flow

---
+++ BlackNectar/BlackNectar/WelcomeScreens/WelcomeScreenFour.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeScreenFour.swift
+//  BlackNectar
+//
+//  Created by Wellington Moreno on 3/3/17.
+//  Copyright © 2017 BlackSource. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WelcomeScreenFour: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ BlackNectar/BlackNectar/WelcomeScreens/WelcomeScreenThree.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeScreenThree.swift
+//  BlackNectar
+//
+//  Created by Wellington Moreno on 3/3/17.
+//  Copyright © 2017 BlackSource. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WelcomeScreenThree: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

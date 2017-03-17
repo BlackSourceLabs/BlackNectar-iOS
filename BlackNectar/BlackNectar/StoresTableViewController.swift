d9f876cc0e77e9ab135b2883f3763e9960365649
Cord <Cordhernandez@yahoo.com>
2017-03-17 10:57:23 -0700

    Adds Aroma Messages

--- BlackNectar/BlackNectar/StoresTableViewController.swift
+++ BlackNectar/BlackNectar/StoresTableViewController.swift
@@ -589,6 +589,43 @@ fileprivate extension StoresTableViewController {
             .send()
     }
     
+    func makeNoteThatUserFinishedEmail(withResult result: String) {
+        
+        LOG.info("Email Result is: \(result)")
+        AromaClient.beginMessage(withTitle: "User Finished Email with: \(result)")
+            .addBody("Finished Email: (\(result))")
+            .withPriority(.low)
+            .send()
+        
+    }
+    
+    func makeNoteThatSendingEmailFailed(withError error: String) {
+        
+        LOG.error("There was an error with the email: \(error)")
+        AromaClient.beginMessage(withTitle: "There was an error with the email: \(error)")
+            .addBody("Email Error is: \(error)")
+            .withPriority(.medium)
+            .send()
+        
+    }
+    
+    func makeNoteThatSendingUserToSettingsForMail() {
+        
+        let message = "Sending User to 'Settings' so they can enable Mail Services"
+        AromaClient.sendMediumPriorityMessage(withTitle: "Sending User To Settings", withBody: message)
+        
+    }
+    
+    func makeNoteThatUserHasMailSettingsDisabled() {
+        
+        LOG.error("User has mail settings disabled")
+        AromaClient.beginMessage(withTitle: "User has mail settings disabled")
+            .addBody("User has mail settings disabled or doesn't have an account")
+            .withPriority(.medium)
+            .send()
+        
+    }
+    
 }
 
 

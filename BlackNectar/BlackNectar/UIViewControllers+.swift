33bea44befc14265c3d3d7e5850db10c117726b1
Cord <Cordhernandez@yahoo.com>
2017-03-20 17:18:43 -0700

    Removes Alert View Code and Moves it to Alerts.swift

--- BlackNectar/BlackNectar/UIViewControllers+.swift
+++ BlackNectar/BlackNectar/UIViewControllers+.swift
@@ -38,78 +38,7 @@ extension UIViewController {
         guard let nav = self.navigationController?.navigationBar else { return }
         nav.isTranslucent = false
     }
-}
-
-//MARK: Alert Controllers 
-
-extension UIViewController {
-    
-    func presentAlert(_ alert: UIAlertController) {
-        
-        self.present(alert, animated: true, completion: nil)
-    }
-    
-    func showAlert(title: String, message: String) {
-        
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        
-        let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
-        alert.addAction(ok)
-        
-        presentAlert(alert)
-    }
-    
-    func showAlertWithError(message: String) {
-        showAlert(title: "Error", message: message)
-    }
-    
-    func createAlertToSendUserToLocationSettings() -> UIAlertController {
-        
-        let title = "Requesting GPS Access"
-        let message = "Please go to \"Location\" and enable \"While Using the App\""
-        
-        let controller = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        
-        let openSettings = UIAlertAction(title: "Open Settings", style: .default) { _ in
-            self.sendUserToSettings()
-            self.makeNoteThatSendingUserToSettings()
-        }
-        
-        controller.addAction(openSettings)
-        
-        return controller
-    }
-    
-    func sendUserToSettings() {
-        
-        let link = UIApplicationOpenSettingsURLString
-        
-        guard let url = URL(string: link) else {
-            LOG.error("Failed to create URL to \(link)")
-            return
-        }
-        
-        UIApplication.shared.open(url, options: [:], completionHandler: nil)
-        
-    }
     
 }
 
-extension UIAlertController {
-    
-    func addActions(_ actions: UIAlertAction...) {
-        
-        actions.forEach(self.addAction)
-    }
-}
 
-//MARK: Aroma Messages
-fileprivate extension UIViewController {
-
-    func makeNoteThatSendingUserToSettings() {
-        
-        let message = "Sending User to 'Settings' so they can enable Location Services"
-        AromaClient.sendMediumPriorityMessage(withTitle: "Sending User To Settings", withBody: message)
-    }
-    
-}

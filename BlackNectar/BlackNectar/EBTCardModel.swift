2a7e8c1536ba46bcf8c6d805fea1c2fcffde5438
Cord <Cordhernandez@yahoo.com>
2017-04-02 05:27:58 -0700

    Imports Archeota

---
+++ BlackNectar/BlackNectar/EBTCardModel.swift
@@ -0,0 +1,11 @@
+//
+//  EBTCardModel.swift
+//  BlackNectar
+//
+//  Created by Cordero Hernandez on 4/2/17.
+//  Copyright © 2017 BlackSource. All rights reserved.
+//
+
+import Archeota
+import Foundation
+import UIKit

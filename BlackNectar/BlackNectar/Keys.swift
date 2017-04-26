da95d83de777b08c117d0e39690b59f8bd2b3418
Cord <Cordhernandez@yahoo.com>
2017-04-25 17:50:39 -0700

    Constructs Code for Keys.swift
    +Static let constants

---
+++ BlackNectar/BlackNectar/Keys.swift
@@ -0,0 +1,30 @@
+//
+//  Keys.swift
+//  BlackNectar
+//
+//  Created by Cordero Hernandez on 4/24/17.
+//  Copyright © 2017 BlackSource. All rights reserved.
+//
+
+import Foundation
+
+class Keys {
+    
+    static let storeID = "store_id"
+    static let storeName = "store_name"
+    static let location = "location"
+    static let latitude = "latitude"
+    static let longitude = "longitude"
+    static let address = "address"
+    static let storeImage = "main_image_url"
+    static let isFarmersMarket = "is_farmers_market"
+    
+    static let addressLineOne = "address_line_1"
+    static let addressLineTwo = "address_line_2"
+    static let city = "city"
+    static let state = "state"
+    static let county = "county"
+    static let zipCode = "zip_code"
+    static let localZipCode = "local_zip_code"
+    
+}
